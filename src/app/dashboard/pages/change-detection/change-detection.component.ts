26dbbdfcb570bf51044f688e67e857bc7c3997f0
Alexiis007 <christianalexis0918@gmail.com>
2024-05-09 08:50:10 -0600

    Actualizacion

---
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './change-detection.component.html',
+  styleUrl: './change-detection.component.css'
+})
+export class ChangeDetectionComponent {
+
+}
---
+++ src/app/shared/heavy-loaders/heavy-loaders-low.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  template: '<h1>Hola mundo</h1>',
+})
+export class HeavyLoadersLowComponent {
+
+}
